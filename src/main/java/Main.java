688dee648b26b27db0acb1cb9247e68f8bcee0f7
kubilaycse <kubilaycse@gmail.com>
2022-04-27 19:46:35 +0300

    signup method added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,31 +5,59 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+
+        String testLink = "https://www.lcwaikiki.com/tr-TR/TR";
+        String getMailError = testSignup(testLink);
+        System.out.println(getMailError);
+    }
+
+    public static String testSignup(String link) {
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.lcwaikiki.com/tr-TR/TR");
+        driver.get(link); // used for getting link
 
+        // Click login button
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         WebElement element1 = driver.findElement(By.cssSelector("a.header-dropdown-toggle"));
         element1.click();
+
+        // Click sign up button
         WebElement element2 = driver.findElement(By.cssSelector("a.login-form__link"));
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         element2.click();
 
 
         WebElement element3 = driver.findElement(By.id("RegisterFormView_RegisterEmail"));
-        element3.sendKeys("deemed");
+        element3.sendKeys("kubilay");
         element3.sendKeys(Keys.TAB);
+
+        // Getting error comes from email
         driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
         WebElement element4 = driver.findElement(By.id("RegisterFormView_RegisterEmail-error"));
-        String s = element4.getText();
-        System.out.println(s);
+        String mailError = element4.getText();
+
+        // not related by the first homework, i just wanted to try
+        List<WebElement> elementList = driver.findElements(By.className("input-checkbox"));
+        for (WebElement elem : elementList) {
+            if(elem.getText().equals("SMS")){
+                elem.click();
+                break;
+            }
+        }
+
+
+        return mailError;
     }
+
+
+
 
 
 }
